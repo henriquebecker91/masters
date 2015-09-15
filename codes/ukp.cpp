7688455ef33608a9313f2d5dc1794ea579cd9404
Henrique Becker <henriquebecker91@gmail.com>
2015-09-15 17:06:51 -0300

    Added a results directory to put the results obtained by each algorithm for every instance. Some results also added. Added cpp file to try to make a faster-than-cplex code.

---
+++ codes/ukp.cpp
@@ -0,0 +1,31 @@
+#include <cstdlib>
+#include <vector>
+#include <istream>
+
+using namespace std;
+
+struct item_t {
+	int w;
+	int p;
+};
+
+struct ukp_instance_t {
+	int c;
+	vector<item_t> items;
+};
+
+void read_ukp_instance(istream in, ukp_instance_t &ukp_i) {
+	while(true) {
+	}
+	return;
+}
+
+void write_ukp_instance(ukp_instance_t &ukp_i, ostream out) {
+	return;
+}
+
+int main (void) {
+
+	return EXIT_SUCCESS;
+}
+

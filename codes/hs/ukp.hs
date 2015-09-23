0f2b6da9ddbe410d29328b512c913561ea6d922c
Henrique Becker <henriquebecker91@gmail.com>
2015-09-23 15:38:57 -0300

    Code in Haskell using the ideia of 'A Sparse knapsack algo-tech-cuit and its synthesis'. If you pass any real world instance to it, it will eat all of your memory.

---
+++ codes/hs/ukp.hs
@@ -0,0 +1,40 @@
+import Data.List (sortBy)
+import Data.String (fromString)
+
+merge :: [(Int, Int)] -> [(Int, Int)] -> [(Int, Int)]
+merge [] ys = ys
+merge xs [] = xs
+merge xs@(i@(w, p):xss) ys@(i'@(w', p'):yss) =
+	if w > w' then i : (merge xss ys)
+	else if w < w' then i' : (merge xs yss)
+	else (w, max p p') : (merge xss yss)
+
+addtest :: (Int, Int) -> [(Int, Int)] -> Int -> [(Int, Int)]
+addtest i@(w, p) [] c = []
+addtest i@(w, p) ((w', p'):xss) c =
+	if w+w' > c then []
+	else (w+w', p+p'):(addtest i xss c)
+
+filter_d :: [(Int, Int)] -> Int -> [(Int, Int)]
+filter_d [] _ = []
+filter_d (i@(w, p):xss) max =
+	if p > max then i : (filter_d xss p)
+	else filter_d xss max
+
+weight_non_decreasing (a1, b1) (a2, b2)
+	| a1 < a2 = GT
+	| a1 > a2 = LT
+	| a1 == a2 = compare b1 b2
+
+s :: Int -> [(Int, Int)] -> Int -> [(Int, Int)]
+s 0 _ _ = []
+s k ys' c = filter_d (merge (s (k-1) ys c) (addtest (ys !! (k-1)) ((0,0):(s k ys c)) c)) 0
+	where ys = sortBy weight_non_decreasing ys'
+
+s' str = (show $ s n ys c) ++ "\n"
+	where	ls = lines str 
+		(n, c) = (read $ ls !! 0, read $ ls !! 1)
+		ys = map (\(w:p:[]) -> (read w, read p)) $ map words $ drop 2 ls
+
+main = interact s'
+

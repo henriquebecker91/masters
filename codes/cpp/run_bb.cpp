f574dfdf7ad705d5df8296d0f3dd890076811337
Henrique Becker <henriquebecker91@gmail.com>
2015-12-07 18:13:42 -0200

    The run and test B&B files were removed (they should be removed two commits ago).

--- codes/cpp/run_bb.cpp
+++
@@ -1,7 +0,0 @@
-#include "bb.hpp"
-#include "test_common.hpp"
-
-int main(int argc, char** argv) {
-    return hbm::main_take_path(&hbm::bb, argc, argv);
-}
-
--- codes/cpp/test_bb.cpp
+++
@@ -1,7 +0,0 @@
-#include "bb.hpp"
-#include "test_common.hpp"
-
-int main(int argc, char** argv) {
-  return hbm::benchmark_pyasukp(&hbm::bb);
-}
-

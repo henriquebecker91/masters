c9ab7a3e8ae38281f900be22d2c86d931aad9478
Henrique Becker <henriquebecker91@gmail.com>
2016-01-02 11:41:44 -0200

    Added script to ease the creation of .res.tukp files.

---
+++ codes/sh/extract_sol.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+# To be used on the run_ukp5 output, it removes the optimal solution items
+# from the rest of the log putting them on a two column format (first
+# weight, second profit).
+
+grep "\(\.\./\|ix\)" "$1" | sed 's/.*w: \([0-9]\+\) p: \([0-9]\+\)/\1 \2/g'
+

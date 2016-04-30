0839319fafe61495a2078d9c6da168300a56b92b
Henrique Becker <henriquebecker91@gmail.com>
2016-04-29 23:06:27 -0300

    Separated the mtu1 wrapper used to call MTU1 from external sources, from the code of the mtu1 that is called from mtu2. The mtu1 is compiling and working. Don't tried compiling mtu2 yet, but the code is finished. Added a note for the Martello book on knapsack problems.

--- codes/cpp/mtu.hpp
+++ codes/cpp/mtu.hpp
@@ -116,6 +116,17 @@ namespace hbm {
       #endif
     #endif //HBM_CHECK_OVERFLOW
 
+    // "Knapsack Problems" p. 93, equation 3.19 
+    template<typename W, typename P>
+    inline P u1(
+      const item_t<W, P> &bi,
+      const item_t<W, P> &bi2,
+      const W &c
+    ) {
+      W c_ = c % bi.w;
+      return (c/bi.w)*bi.p + (c_*bi2.p)/bi2.w;
+    }
+
     template<typename W, typename P>
     inline P u3(
       const item_t<W, P> &bi,
@@ -156,6 +167,117 @@ namespace hbm {
       return max(u1_, u0); // 3.26
     }
 
+    // TODO: write short note
+    // Note that this method has index notation beggining on 1.
+    // It's expected that the size of w and p exceeds n by at least one
+    // position (i.e. w and p have at least size n+1).
+    template<typename W, typename P, typename I>
+    void inner_mtu1(
+      myvector<W> &w,
+      myvector<P> &p,
+      const I &n,
+      const W &c,
+      P &z,
+      myvector<W> &x
+    ) {
+      const item_t<W, P> bi(w[1], p[1]), bi2(w[2], p[2]), bi3(w[3], p[3]);
+
+      W y, i;
+      P u;
+      I h;
+
+      // 1. Initialize
+      //step_1: // unused step
+      z = 0;
+      P z_ = 0;
+      W c_ = c;
+
+      // TODO: At the end verify if this can be done without adding this
+      // dummy item.
+      p[n+1] = 0;
+      w[n+1] = numeric_limits<W>::max();
+
+      vector<W> x_(n+1, 0);
+      P upper_u = u3(bi, bi2, bi3, c);
+
+      myvector<W> m;
+      m.resize(n+1); // This resize don't initialize the m contents with zero
+
+      m[n] = w[n + 1]; // Changing the dummy item would change there
+      for (I k = n - 1; k > 0; --k) m[k] = min(m[k + 1], w[k + 1]);
+
+      I j = 1;
+
+      // 2. build a new current solution
+      step_2:
+      while (w[j] > c_)
+        if (z >= z_ + (c_*p[j+1])/w[j+1]) goto step_5; else j = j + 1;
+
+      y = c_/w[j];
+      u = ((c_ - y*w[j])*p[j+1])/w[j+1];
+
+      if (z >= z_ + y*p[j] + u) goto step_5;
+      if (u == 0) goto step_4;
+
+      // 3. save the current solution
+      //step_3: // unused step
+      c_ = c_ - y*w[j];
+      z_ = z_ + y*p[j];
+      x_[j] = y;
+      j = j + 1;
+      if (c_ >= m[j-1]) goto step_2;
+      if (z >= z_) goto step_5;
+      y = 0;
+
+      // 4. update the best solution so far
+      step_4:
+      z = z_ + y*p[j];
+      for (I k = 1; k < j; ++k) x[k] = x_[k];
+      x[j] = y;
+      for (I k = j + 1; k <= n; ++k) x[k] = 0;
+      if (z == upper_u) goto stop;
+
+      // 5. backtrack
+      step_5:
+      i = 0; // We use zero as an sentinel value
+      for (I k = j - 1; k > 0; --k) if (x_[k] > 0) { i = k; break; }
+      if (i == 0) goto stop; // "if no such i then return"
+      c_ = c_ + w[i];
+      z_ = z_ - p[i];
+      x_[i] = x_[i] - 1;
+      if (z >= z_ + (c_*p[i+1])/w[i+1]) {
+        c_ = c_ + w[i]*x_[i];
+        z_ = z_ - p[i]*x_[i];
+        x_[i] = 0;
+        j = i;
+        goto step_5;
+      }
+      j = i + 1;
+      if (c_ - w[i] >= m[i]) goto step_2;
+      h = i;
+
+      // 6. try to replace one item of type i with items of type h
+      step_6:
+      h = h + 1;
+      if (z >= z_ + (c_*p[h])/w[h]) goto step_5;
+      if (w[h] == w[i]) goto step_6;
+      if (w[h] > w[i] ) {
+        if (w[h] > c_ || z >= z_ + p[h]) goto step_6;
+        z = z_ + p[h];
+        for (I k = 1; k <= n; ++k) x[k] = x_[k];
+        x[h] = 1;
+        if (z == upper_u) goto stop;
+        i = h;
+        goto step_6;
+      } else {
+        if (c_ - w[h] < m[h-1]) goto step_6;
+        j = h;
+        goto step_2;
+      }
+
+      stop:;
+    }
+
     // TODO: quick comment on the method
     template<typename W, typename P, typename I>
     void mtu1(instance_t<W, P> &ukpi, solution_t<W, P, I> &sol, bool already_sorted) {
@@ -181,23 +303,28 @@ namespace hbm {
       }
 
       // Put items on article's notation
-      vector<W> w;
-      w.reserve(n + 2);
-      vector<P> p;
-      p.reserve(n + 2);
-      w.push_back(0); // not exist notation begins at 1
-      p.push_back(0); // not exist notation begins at 1
-      for (auto &it : items) {
-        w.push_back(it.w);
-        p.push_back(it.p);
+      myvector<W> w;
+      w.resize(n + 2);
+      w[0] = 0; // position does not exist, notation begins at 1
+      myvector<P> p;
+      p.resize(n + 2);
+      p[0] = 0; // position does not exist, notation begins at 1
+      item_t<W, P> it;
+      for (I i = 0, j = 1; i < n; ++i, ++j) {
+        it = items[i];
+        w[j] = it.w;
+        p[j] = it.p;
       }
 
       // We need to initialize some variables before we start jumping.
-      W y, i;
-      P u;
-      I h;
       myvector<W> x;
       x.resize(n+1); // This resize don't initialize the m contents with zero
+
+      P z;
+      inner_mtu1(w, p, n, c, z, x);
+      /*W y, i;
+      P u;
+      I h;
 
       // 1. Initialize
       //step_1: // unused step
@@ -285,7 +412,7 @@ namespace hbm {
         if (c_ - w[h] < m[h-1]) goto step_6;
         j = h;
         goto step_2;
-      }
+      }*/
 
       stop:
       sol.opt = 0;
@@ -314,16 +441,126 @@ namespace hbm {
       // Used to compute the time all the execution algorithm.
       steady_clock::time_point all_mtu2_begin = steady_clock::now();
 
-      const W c = ukpi.c;
-      const I n = ukpi.items.size();
-      I k;
+      const W c = eip->c = ukpi.c;
+      const I n = eip->n = ukpi.items.size();
+      auto &items = ukpi.items;
+
+      P z;
       // v is the size the core problem begins and grows at each iteration.
       // TODO: MAKE THIS A PARAMETER
-      I v = 100;
-      do {
-        k += min(k + v, n);
+      if (n < 100) {
+        mtu1(ukpi, sol, already_sorted);
+        return;
+      }
+      I v = max(100, n/100);
 
-      } while ();
+      if (!already_sorted) {
+        // The G, E, and overlined E sets are unecessary. The code does the
+        // almost the same as partial ordering the first 'v' elements.
+        sort_k_most_eff(items.begin(), items.end(), v);
+      }
+
+      // The core will be kept a profit and a weight array, to avoid having
+      // to convert for each call to inner_mtu1. The arrays are of size 'n + 2'
+      // because: 1º) notation starts at 1; 2º) inner_mtu1 adds a dummy
+      // item at the end.
+      myvector<W> core_p;
+      core_p.resize(n + 2); // this resize don't initialize any values
+      core_p[0] = 0; // position does not exist, notation begins at 1
+      myvector<P> core_w;
+      core_w.resize(n + 2); // this resize don't initialize any values
+      core_w[0] = 0; // position does not exist, notation begins at 1
+      item_t<W, P> aux_item;
+      for (I i = 0, j = 1; i < v; ++i, ++j) {
+        aux_item = items[i];
+        core_w[j] = aux_item.w;
+        core_p[j] = aux_item.p;
+      }
+
+      // MTU2 describe some steps on a high level. We tried to implemet those
+      // on the most efficient fashion possible. One of the high-level steps
+      // that are hard to manage in an efficient fashion is keeping non_core
+      // update, it have elements removed frequently (from front and any
+      // position in the middle of it).
+      myvector< item_t<W, P> > non_core;
+      non_core.resize(n - v);
+      const size_t qt_bytes_to_copy = non_core.size()*sizeof(item_t<W, P>);
+      memcpy(non_core.data(), items.data() + v, qt_bytes_to_copy);
+      // The vector<bool> isn't ideal here, as it trades memory consumption for
+      // time. And we are optimizing this algorithm for time.
+      myvector<char> items_to_remove(non_core.size(), 0);
+      size_t qts_item_to_remove = 0;
+      myvector< item_t<W, P> > aux_non_core;
+
+      // The solution vector.
+      myvector<W> x;
+      x.resize(n + 1);
+      memset(x.data(), 0, (v+1)*sizeof(W));
+
+      inner_mtu1(core_w, core_p, v, c, z, x);
+
+      P upper_u3 = u3(items[0], items[1], items[2], c);
+
+      I non_core_start = 0;
+      size_t non_core_size = non_core.size();
+
+      I k = v;
+      while (z != upper_u3 && non_core.size() > 0) {
+        P u, pj;
+        W wj;
+        for (size_t j = non_core_start; j < non_core_size; ++j) {
+          pj = non_core[j].p;
+          wj = non_core[j].w;
+          u = pj + u1(items[0], items[1], c - wj);
+          if (u > z) u = pj + u3(items[0], items[1], items[2], c - wj);
+          // the 'if' below can't be an 'else' of the 'if' above, don't try
+          // the variable 'u' changes value on the 'if' above
+          if (u <= z) {
+            items_to_remove[j] = true;
+            ++qt_items_to_remove;
+          }
+        }
+
+        // Here we remove the dominated items from non_core (together with
+        // the v first items that were added to core, and that are not part
+        // of non_core anymore), and update the relevant variables.
+        {
+          aux_non_core.resize(non_core_size - qt_items_to_remove);
+
+          for (size_t j = non_core_start; j < non_core_size; ++j) {
+            if (!items_to_remove[j]) { 
+              aux_non_core[j - non_core_start] = non_core[j];
+            }
+          }
+
+          non_core.swap(aux_non_core);
+          non_core_start = 0;
+          non_core_size = non_core.size();
+          memset(items_to_remove.data(), 0, non_core_size*sizeof(char));
+          qt_items_to_remove = 0;
+        }
+
+        // Now we get the v most efficient items on non_core, and add them to
+        // the core. We do not remove those items of non_core, but we set
+        // non_core_start that will remove them for us on the next loop.
+        if (!already_sorted) {
+          sort_k_most_eff(non_core.begin(), non_core.end(), v);
+        }
+        const I next_slice_size = min(v, non_core_size);
+        for (I i = 0; i < next_slice_size; ++i, ++k) {
+          aux_item = items[i];
+          core_w[k] = aux_item.w;
+          core_p[k] = aux_item.p;
+        }
+        non_core_start = v;
+
+        // We do not need to clean the x solution array, inner_mtu1 already
+        // does this.
+        //memset(x.data(), 0, k*sizeof(W));
+        z = 0;
+
+        inner_mtu1(core_w, core_p, k, c, z, x);
+      } 
 
       // Used by HBM_START_TIMER and HBM_STOP_TIMER if HBM_PROFILE is defined.
       steady_clock::time_point begin;

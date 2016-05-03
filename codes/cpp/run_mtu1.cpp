440fbbd797f4ec83302619863aa427f4d5277a8d
Henrique Becker <henriquebecker91@gmail.com>
2016-05-03 11:15:55 -0300

    Added comments to the MTU1 '.cpp' files.

--- codes/cpp/run_mtu1.cpp
+++ codes/cpp/run_mtu1.cpp
@@ -1,6 +1,9 @@
 #include "mtu.hpp"
 #include "test_common.hpp"
 
+// Solves an UKP instance by the mtu1 algorithm (created by Martello and Toth
+// and described in the book "Knapsack Problems" p. 96). Takes the name of a
+// file in the ".ukp" format. Other options should be consulted at mtu.hpp.
 int main(int argc, char** argv) {
   return hbm::main_take_path<size_t, size_t, size_t>(&hbm::mtu1, argc, argv);
 }
--- codes/cpp/test_mtu1.cpp
+++ codes/cpp/test_mtu1.cpp
@@ -1,6 +1,9 @@
 #include "mtu.hpp"
 #include "test_common.hpp"
 
+// Execute mtu1 algorithm (created by Martello and Toth and described in the
+// book "Knapsack Problems" p. 96) over a hardcoded set of instances. Used to
+// check if it is still working after a change.
 int main(int argc, char** argv) {
   std::cout << "hbm::benchmark_pyasukp<size_t, size_t, size_t>(&hbm::mtu1, argc, argv)" << std::endl;
   return hbm::benchmark_pyasukp<size_t, size_t, size_t>(&hbm::mtu1, argc, argv);

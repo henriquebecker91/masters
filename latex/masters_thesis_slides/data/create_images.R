9da05a645e69d0309c1843c92702bb4508fe44ea
Henrique Becker <henriquebecker91@gmail.com>
2016-12-07 18:18:00 -0200

    Forgotten line.

--- latex/masters_thesis_slides/data/create_images.R
+++ latex/masters_thesis_slides/data/create_images.R
@@ -4,7 +4,8 @@ for (name in c('hi_n5000-0-s731232778c5052835',
               'nsds2_n50000wmin50000-0-s155213243c1070927',
               'saw_n10000wmin10000-0-s985850175c608451',
               'sc_a5n10000wmin10000-0-c6020768',
-              'ss2_wmin1000wmax1000000n10000-0-s827183242c7523725')) {
+              'ss2_wmin1000wmax1000000n10000-0-s827183242c7523725',
+              '128_16_std_breqd-n16384-s0')) {
   csv <- paste(name, '.csv', sep = '')
   image <- paste(name, '.pdf', sep = '')
   

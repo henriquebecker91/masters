0bb1e01161442e37e64b4eb22d5243b70e09dfea
Henrique Becker <henriquebecker91@gmail.com>
2016-12-07 18:16:30 -0200

    Created script for generating instance graphs in the right size.

---
+++ latex/masters_thesis_slides/data/create_images.R
@@ -0,0 +1,17 @@
+library(ggplot2)
+
+for (name in c('hi_n5000-0-s731232778c5052835',
+              'nsds2_n50000wmin50000-0-s155213243c1070927',
+              'saw_n10000wmin10000-0-s985850175c608451',
+              'sc_a5n10000wmin10000-0-c6020768',
+              'ss2_wmin1000wmax1000000n10000-0-s827183242c7523725')) {
+  csv <- paste(name, '.csv', sep = '')
+  image <- paste(name, '.pdf', sep = '')
+  
+  t <- read.csv(csv, sep = ';')
+  ggplot(t, aes(x = w, y = p)) +
+    geom_point() +
+    xlab('weight') + 
+    ylab('profit')
+  ggsave(image, units = 'cm', width = 10, height = 7.5)
+}

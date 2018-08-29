f86a087b2060d0c3522383ee115697385832870a
Henrique Becker <henriquebecker91@gmail.com>
2018-08-29 16:16:13 -0300

    WIP. Knapsack model in Gurobi almost done.

--- codes/cpp/lib/gurobi_ukp_model.hpp
+++ codes/cpp/lib/gurobi_ukp_model.hpp
@@ -1,13 +1,17 @@
 #ifndef HBM_GUROBI_UKP_MODEL_HPP
 #define HBM_GUROBI_UKP_MODEL_HPP
 
-#include <memory> // for unique_ptr
+#include <memory>
+#include <cassert>
+#include <cmath>
 #include "gurobi_c++.h"
 #include "ukp_common.hpp"
 
 #ifndef HBM_PRINT_VAR
   #define HBM_PRINT_VAR(var) out << #var ": " << var << std::endl
 #endif
+
+#define HBM_TOLERANCE 0.00001
 
 namespace hbm {
   template<typename W, typename P, typename I>
@@ -19,54 +23,38 @@ namespace hbm {
   ) {
     using namespace std;
 
+    const I n = static_cast<I>(ukpi.items.size());
+    const double c = ukpi.c;
+
+    unique_ptr<double[]> w(new double[n]);
+    unique_ptr<double[]> p(new double[n]);
+
+    for (I i = 0; i < n; ++i) {
+      w[i] = static_cast<double>(ukpi.items[i].w);
+      p[i] = static_cast<double>(ukpi.items[i].p);
+    }
+
     try {
       GRBEnv env = GRBEnv();
       GRBModel model = GRBModel(env);
-      // Creates capacity constraint with only the first variable.
-      const w0 = static_cast<double>(ukpi.items[0].w);
-      const p0 = static_cast<double>(ukpi.items[0].p);
-      GRBConstr capacity = model.addConstr(
-        w0 * model.addVar(0.0, ukpi.c/w0, p0, GRB_INTEGER),
-        GRB_LESS_EQUAL,
-        static_cast<double>(ukpi.c)
-      );
-      // CREATE THE FIRST VARIABLE FIRST, AND THEN OBJECTIVE, AND THEN CONSTRAINT,
-      // AN THEN ALL OTHER VARIABLES LINKED TO OBJECTIVE AND CONSTRAINT
-      model.setObjective(, GRB_MAXIMIZE);
+      unique_ptr<GRBVar[]> x(model.addVars(n, GRB_INTEGER));
+      GRBLinExpr objective_expr, capacity_lhs_expr;
+
+      objective_expr.setTerms(p, x, n);
+      model.setObjective(objective_expr, GRB_MAXIMIZE);
+      capacity_lhs_expr.setTerms(w, x, n);
+      model.addConstr(capacity_lhs_expr, GRB_LESS_EQUAL, c);
       
-      //std::unique_ptr< GRBVar[] > vars = new GRBVar[ukpi.n];
-      double wi, pi;
-      for (I i = 1; i < n; ++i) {
-        wi = static_cast<double>(ukpi.items[i].w);
-        pi = static_cast<double>(ukpi.items[i].p);
-        static_cast<void>(
-          model.addVar(0.0, ukpi.c/wi, pi, GRB_INTEGER, 1, &capacity, &wi)
-        );
-      }
-
-      model.setObjective(x + y + 2 * z, GRB_MAXIMIZE);
-
-      // Add constraint: x + 2 y + 3 z <= 4
-
-      model.addConstr(x + 2 * y + 3 * z <= 4, "c0");
-
-      // Add constraint: x + y >= 1
-
-      model.addConstr(x + y >= 1, "c1");
-
-      // Optimize model
-
       model.optimize();
 
-      cout << x.get(GRB_StringAttr_VarName) << " "
-           << x.get(GRB_DoubleAttr_X) << endl;
-      cout << y.get(GRB_StringAttr_VarName) << " "
-           << y.get(GRB_DoubleAttr_X) << endl;
-      cout << z.get(GRB_StringAttr_VarName) << " "
-           << z.get(GRB_DoubleAttr_X) << endl;
-
-      cout << "Obj: " << model.get(GRB_DoubleAttr_ObjVal) << endl;
-
+      sol.opt = static_cast<P>(model.get(GRB_DoubleAttr_ObjVal) + HBM_TOLERANCE);
+      for (I i = 0; i < n; ++i) {
+        double xi = x[i].get(GRB_DoubleAttr_X);
+        assert(abs(xi - static_cast<int>(xi + HBM_TOLERANCE)) < HBM_TOLERANCE);
+        if (xi >= (1.0 - HBM_TOLERANCE)) sol.used_items.emplace_back(
+          ukpi[i], static_cast<I>(xi + HBM_TOLERANCE), i
+        );
+      }
     } catch(GRBException e) {
       cout << "Error code = " << e.getErrorCode() << endl;
       cout << e.getMessage() << endl;
